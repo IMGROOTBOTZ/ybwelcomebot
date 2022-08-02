a8fce2ab358e17c80c0409948b5b0ac393b764cc
IMGROOTBOTZ <110440328+IMGROOTBOTZ@users.noreply.github.com>
2022-08-02 11:24:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ def start(updater,context):
  updater.message.reply_text('''Hi iam welcome messanger bot 
 Add me to your group 
  
- Made with Love ❤️ by @tamilan444
+ Made with Love ❤️ by @iamgrootbotz
 
   ''')
 def help(updater,context):
